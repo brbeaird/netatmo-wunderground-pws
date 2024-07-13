0c554be32c36e8aae3f8458c6df04cd91e717d07
Brian Beaird <brbeaird@gmail.com>
2024-07-13 12:52:53 -0500

    Add support for new oauth flow

--- netatmo-wunderground-pws.js
+++ netatmo-wunderground-pws.js
@@ -1,9 +1,11 @@
-
-var netatmo = require('netatmo')
+const {NetatmoApiClient} = require('netatmo-api-client');
+const fs = require('node:fs/promises');
+const path = require('path');
 var PWS = require('wunderground-pws');
 
 var netatmoAuth;
 var wundergroundAuth;
+var netatmoClient;
 
 var netatmo_pws = function (args) {
     this.setAuthVars(args);
@@ -13,19 +15,21 @@ netatmo_pws.prototype.setAuthVars = function(args) {
     netatmoAuth = {
         "client_id": args.netamo_client_id,
         "client_secret": args.netamo_client_secret,
-        "username": args.netamo_username,
-        "password": args.netamo_password,
+        "accessToken": args.netamo_accessToken,
+        "refreshToken": args.netamo_refreshToken,
+        "tokenFileDirectory": args.netamo_tokenFileDirectory
     };
     wundergroundAuth = {
         "wundergroundStationId": args.wundergroundStationId,
         "wundergroundUserPassword": args.wundergroundUserPassword
     };
+    netatmoClient = new NetatmoApiClient(netatmoAuth.client_id, netatmoAuth.client_secret);
 }
 
 var pws;
-var api;
 
 //Data vars
+var stationData;
 var winddirection;
 var windspeed;
 var windgust;
@@ -38,66 +42,94 @@ var baromin;
 var softwaretype=  'netatmo-wunderground-pws';
 
 //Get data from Netatmo weather station
-netatmo_pws.prototype.getNetatmoData = function (){
+netatmo_pws.prototype.getNetatmoData = async function (){
     try {
-        api = new netatmo(netatmoAuth);
-
-        api.on("error", function(error) {
-            console.error('Netatmo threw an error: ' + error);
-        });
-
-        api.on("warning", function(error) {
-            console.log('Netatmo threw a warning: ' + error);
-        });
 
 
-        console.debug("Getting Netatmo data...");
-        api.getStationsData(function(err, devices) {
-            let dev = devices[0];
-            baromin = dev.dashboard_data.Pressure * 0.0295299830714;
+        const tokenFilePath = path.join(netatmoAuth.tokenFileDirectory, 'auth.json');
 
-            for (let mod of dev.modules){
-                if (mod.type == "NAModule1"){   //Outdoor module
-                    if (mod.reachable){
-                        console.debug("Got outdoor data...");
-                        let data = mod.dashboard_data;
-                        tempf = convertFromCtoF(data.Temperature);
-                        humidity = data.Humidity;
-                        dewptf = (data.Temperature - (14.55 + 0.114 * data.Temperature) * (1 - (0.01 * data.Humidity)) - Math.pow((2.5 + 0.007 * data.Temperature) * (1 - (0.01 * data.Humidity)), 3) - (15.9 + 0.117 * data.Temperature) * Math.pow(1 - (0.01 * data.Humidity), 14));
-                        dewptf = convertFromCtoF(dewptf);
-                    }
-                    else{
-                        console.error("Outdoor module is unreachable.");
-                    }
-                }
-                else if (mod.type == "NAModule3"){  //Rain module
-                    if (mod.reachable){
-                        console.debug("Got rain module data...");
-                        let data = mod.dashboard_data;
-                        rainin = convertFromMmtoIn(data.sum_rain_1);
-                        dailyrainin = convertFromMmtoIn(data.sum_rain_24);
-                    }
-                    else{
-                        console.error("Rain module is unreachable.");
-                    }
-                }
-                else if (mod.type == "NAModule2"){  //Wind module
-                    if (mod.reachable){
-                        console.debug("Got wind module data...");
-                        let data = mod.dashboard_data;
-                        winddirection = data.WindAngle;
-                        windspeed = convertFromKphToMph(data.WindStrength);
-                        windgust = convertFromKphToMph(data.GustStrength);
-                    }
-                    else{
-                        console.error("Wind module is unreachable.");
-                    }
+        //If saved tokens exist, try them first
+        if (!netatmoClient.accessToken){
+            if (await exists(tokenFilePath)) {
+                try {
+                    const authFileData = await fs.readFile(tokenFilePath, 'utf-8');
+                    const savedAuthInfo = JSON.parse(authFileData);
+                    netatmoClient.setTokens(savedAuthInfo.accessToken, savedAuthInfo.refreshToken);
+                    stationData = await netatmoClient.getStationData();
+                    log('Succesfully logged in with saved tokens');
+                } catch (error) {
+                    log(`Failed to login with saved tokens (${error.message}). Trying again with environment variables.`, 1);
+                    netatmoClient.accessToken = null;
                 }
             }
-            setObservations();
-        });
+
+            //Fallback to startup environment variables
+            if (!netatmoClient?.accessToken){
+                try {
+                    netatmoClient.setTokens(netatmoAuth.accessToken, netatmoAuth.refreshToken);
+                    stationData = await netatmoClient.getStationData();
+                    log('Succesfully logged in with environment variables');
+                } catch (error) {
+                    log(`Failed to login with environment variables (${error.message}). Please get fresh tokens from the Netatmo Dev tool`, 1);
+                }
+            }
+        }
+
+        const accessDetails = {
+            accessToken: netatmoClient.accessToken,
+            refreshToken: netatmoClient.refreshToken
+        }
+
+        //Save tokens to file
+        const tokenDirectoryExists = await exists(netatmoAuth.tokenFileDirectory)
+        if (!tokenDirectoryExists){
+            await fs.mkdir(netatmoAuth.tokenFileDirectory);
+        }
+        await fs.writeFile(tokenFilePath, JSON.stringify(accessDetails));
+
+
+        let dev = stationData.devices[0];
+        baromin = dev.pressure.current * 0.0295299830714;
+
+        for (let mod of dev.modules){
+            if (mod.type == "OUTDOOR_MODULE"){   //Outdoor module
+                if (mod.reachable){
+                    log("Got outdoor data...");
+                    let tempCelsius = mod.temperature.current;
+                    tempf = convertFromCtoF(tempCelsius);
+                    humidity = mod.humidity;
+                    dewptf = (tempCelsius - (14.55 + 0.114 * tempCelsius) * (1 - (0.01 * humidity)) - Math.pow((2.5 + 0.007 * tempCelsius) * (1 - (0.01 * humidity)), 3) - (15.9 + 0.117 * tempCelsius) * Math.pow(1 - (0.01 * humidity), 14));
+                    dewptf = convertFromCtoF(dewptf);
+                }
+                else{
+                    log("Outdoor module is unreachable.", 1);
+                }
+            }
+            else if (mod.type == "RAIN_MODULE"){  //Rain module
+                if (mod.reachable){
+                    log("Got rain module data...");
+                    rainin = convertFromMmtoIn(mod.rain.current);
+                    dailyrainin = convertFromMmtoIn(mod.rain.last24Hours);
+                }
+                else{
+                    log("Rain module is unreachable.", 1);
+                }
+            }
+            else if (mod.type == "WIND_MODULE"){  //Wind module
+                if (mod.reachable){
+                    log("Got wind module data...");
+                    winddirection = mod.wind.windAngle;
+                    windspeed = convertFromKphToMph(mod.wind.windStrength);
+                    windgust = convertFromKphToMph(mod.wind.gustStrength);
+                }
+                else{
+                    log("Wind module is unreachable.", 1);
+                }
+            }
+        }
+        setObservations();
     } catch (error) {
-        console.error(error.message);
+        log(error.message, 1);
     }
 }
 
@@ -118,14 +150,14 @@ function setObservations(){
     try {
         pws = undefined;
         pws = new PWS(wundergroundAuth.wundergroundStationId, wundergroundAuth.wundergroundUserPassword);
-        console.debug("Sending to Weather Underground...");
-        console.debug("Temp: " + tempf);
-        console.debug("Humidity: " + humidity);
-        console.debug("DewPt: " + dewptf);
-        console.debug("Windspeed: " + windspeed);
-        console.debug("WindGust: " + windgust);
-        console.debug("rain: " + rainin);
-        console.debug("dailyRain: " + dailyrainin);
+        log("Sending to Weather Underground...");
+        log("Temp: " + tempf);
+        log("Humidity: " + humidity);
+        log("DewPt: " + dewptf);
+        log("Windspeed: " + windspeed);
+        log("WindGust: " + windgust);
+        log("rain: " + rainin);
+        log("dailyRain: " + dailyrainin);
         pws.resetObservations();
         pws.setObservations({
             winddir: winddirection,
@@ -142,15 +174,36 @@ function setObservations(){
 
         pws.sendObservations(function(err, success){
             if (err){
-                console.error("Error sending data to Weather Underground: " + err.message);
+                log("Error sending data to Weather Underground: " + err.message, 1);
             }
             else{
-                console.debug("Data successfully sent!");
+                log("Data successfully sent!");
             }
 
         });
     } catch (error) {
-        console.error(error.message)
+        log(error.message, 1)
+    }
+}
+
+//Check if file exists
+async function exists(f) {
+    try {
+        await fs.stat(f);
+        return true;
+    } catch (e) {
+        return false;
+    }
+  }
+
+//Logging with timestamp
+function log(msg, isError) {
+    let dt = new Date().toLocaleString();
+    if (!isError) {
+        console.log(dt + ' | ' + msg);
+    }
+    else{
+        console.error(dt + ' | ' + msg);
     }
 }
 
